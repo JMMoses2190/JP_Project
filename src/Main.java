c3e34a6f6df19aea6d0303c35bb8a97b596030f8
Jordan Moses <jmmoses2190@eagle.fgcu.edu>
2018-11-02 15:01:03 -0400

    Updated Alpha

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,23 @@ import java.util.Date;
 
 public class Main {
 
-    public static void main(String args[]) {
+  public static void main(String args[]) {
 
-      Widget w1 = new Widget("Widget 1");
-      System.out.println(w1.toString());
-      Widget w2 = new Widget("Widget 2");
-      System.out.println(w2.toString());
+    Widget w1 = new Widget("Widget 1");
+    System.out.println(w1.toString());
+    Widget w2 = new Widget("Widget 2");
+    System.out.println(w2.toString());
 
-
-
+    for (ItemType it : ItemType.values()) {
+      System.out.println(it + " " + it.code);
     }
+  }
 
   class Widget extends Product {
-      public Widget(String name) {
-        super(name);
-      }
 
-      @Override
-    public Date getManufactureDate() {
-        return null;
-      }
-
-      @Override
-    public void setManufacturer(String manufacturer){
-
-      }
-  }
+    public Widget(String name) {
+      super(name);
+    }
   }
 
+}
